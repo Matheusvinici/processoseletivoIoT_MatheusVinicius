fa6418227b62cb9bebdb045cb4dce1d6cc4a9b8d
Matheus Vinicius <matheus2vandrade@gmail.com>
2026-07-24 12:21:57 -0300

    use SPI-based HX711 driver (bit-bang incompatible with Wokwi)

--- src/main.py
+++ src/main.py
@@ -1,33 +1,13 @@
-from machine import Pin
 import time
+from hx711_spi import HX711
 
-class HX711:
-    def __init__(self, dout_pin, sck_pin):
-        self.dout = Pin(dout_pin, Pin.IN)
-        self.sck = Pin(sck_pin, Pin.OUT, value=0)
-
-    def read(self):
-        value = 0
-        for _ in range(24):
-            self.sck.value(1)
-            time.sleep_us(20)
-            value = (value << 1) | self.dout.value()
-            self.sck.value(0)
-            time.sleep_us(20)
-        self.sck.value(1)
-        time.sleep_us(20)
-        self.sck.value(0)
-        time.sleep_us(20)
-        if value > 0x7fffff:
-            value -= 0x1000000
-        return value
+sensor = HX711(23, 19, gain=128)
 
 EMPTY_THRESHOLD = 200
 FULL_WEIGHT = 5000
 READ_INTERVAL_MS = 500
 REPORT_INTERVAL_MS = 2000
 
-sensor = HX711(19, 18)
 last_read = 0
 last_report = 0
 state = "NORMAL"
@@ -41,7 +21,7 @@ while True:
     if time.ticks_diff(now, last_read) >= READ_INTERVAL_MS:
         last_read = now
         raw = sensor.read()
-        if 0 <= raw <= 100000:
+        if raw is not None and 0 <= raw <= 100000:
             weight = raw
 
             if weight == 0:
